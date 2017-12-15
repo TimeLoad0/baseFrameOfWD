0f9d13505a641239ca6c075984598bef8fe68f3a
TimeLoad0 <Time_Load@users.noreply.github.com>
2017-12-15 10:35:08 +0800

    添加公共方法

--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -8,11 +8,21 @@
     var tableOptions = {
         cells:[
             {text:"cell1",field:"c1"},
-            {text:"cell2",field:"c2"},
+            {text:"cell2",field:"c2",display:false},
             {text:"cell3",field:"c3"}
         ]};
 
-    var data = [{"c1":"aa1","c2":"bb1","c3":"cc1"},{"c1":"aa2","c2":"bb2","c3":"cc2"},{"c1":"aa3","c2":"bb3","c3":"cc3"}];
+    var data = {
+        success:true,
+        message:"success",
+        pageSize:10,
+        pageNo:1,
+        totalSize:3,
+        dataRows:[
+            {"c1":"aa1","c2":"bb1","c3":"cc1"},
+            {"c1":"aa2","c2":"bb2","c3":"cc2"},
+            {"c1":"aa3","c2":"bb3","c3":"cc3"}
+        ]};
 
     bindTable(tableOptions,data);
 </script>
--- src/main/webapp/js/util/common.js
+++ src/main/webapp/js/util/common.js
@@ -5,9 +5,10 @@
  * wj
  */
 var _tableOptions = {}; //表格选项
+var _data = {}; //数据缓存
 
 //创建绑定表格
-function bindTable(options,data){
+function bindTable(options,data) {
     //默认选项
     var _defaultOptions = {
         add:true,           //新增，默认true
@@ -20,26 +21,25 @@ function bindTable(options,data){
     //合并选项
     _tableOptions = $.extend(true,_defaultOptions,options);
 
-    var cells = _defaultOptions.cells;
-
     //创建表格
     var table = '<table id="dataTable" class="table table-striped">';
     $('#mainBody').html(table);
 
     //创建表头
-    createThead(_defaultOptions.cells);
+    createThead(_tableOptions.cells);
     //绑定表格数据
     createTbody(data);
 }
 
 //创建表头
-function createThead(cells){
+function createThead(cells) {
     var thead = '<thead><tr>';
 
     //循环cells创建表头列
-    for(var i=0;i<cells.length;i++){
-        var th = '<th field="'+cells[i].field+'">'+cells[i].text+'</th>';
-        thead += th;
+    for(var i=0;i<cells.length;i++) {
+        var display = !nullToTrue(cells[i].display)?"display:none;":"";
+
+        thead += '<th style="'+display+'" field="' + cells[i].field + '">' + cells[i].text + '</th>';
     }
 
     thead += '</tr></thead>';
@@ -48,24 +48,25 @@ function createThead(cells){
 }
 
 //绑定表格数据
-function createTbody(data){
-    if(undefined == data || "" == data || data.length <= 0){
-        return;
+function createTbody(data) {
+    if(isEmpty(data) || data.length <= 0) {
+        return false;
     }
 
-    //获取表头html
-    var thead = $('#dataTable').html();
-    var tbody = '<tbody>';
+    _data = data; //缓存数据data
 
-    var ths = $('#dataTable thead tr th');
+    var dataRows = data.dataRows; //获取数据行
+    var dataTable = $('#dataTable'); //获取dataTable对象
+    var thead = dataTable.html(); //获取表头html
+    var ths = dataTable.find('thead tr th'); //获取表头所有th
+    var tbody = '<tbody>'; //tbody对象
 
-    for(var j=0;j<data.length;j++){
+    for(var i=0;i<dataRows.length;i++) {
         var tr = '<tr>';
 
         //循环表头
-        for(var k=0;k<ths.length;k++){
-            var td = '<td>'+data[j][$(ths[k]).attr('field')]+'</td>';
-            tr += td;
+        for(var j=0;j<ths.length;j++) {
+            tr += '<td rowData=' + JSON.stringify(dataRows[i]) + '>' + dataRows[i][$(ths[j]).attr('field')] + '</td>';
         }
 
         tr += '</tr>';
@@ -74,6 +75,184 @@ function createTbody(data){
 
     tbody += '</tbody>';
 
-    $('#dataTable').html(thead + tbody);
+    dataTable.html(thead + tbody);
 }
 
+//空转空字符串
+function nullToEmpty(obj) {
+    if ( undefined == obj || null == obj ) {
+        obj = "";
+    }
+
+    return obj;
+}
+
+//空转false
+function nullToFalse(obj) {
+    if ( undefined == obj || null == obj ) {
+        obj = false;
+    }
+
+    return obj;
+}
+
+//空转true
+function nullToTrue(obj) {
+    if ( undefined == obj || null == obj ) {
+        obj = true;
+    }
+
+    return obj;
+}
+
+/** 把一个浮点数，以小数点后几位四舍五入
+ *   @param srcValue 要舍位的值
+ *   @param iCount  要舍位到小数点后几位
+ *   @return number
+ */
+function round(srcValuef, iCount) {
+    var srcValue = srcValuef;
+
+    var zs = true;
+
+    //判断是否是负数
+    if ( srcValue < 0 ) {
+        srcValue = Math.abs(srcValue);
+        zs = false;
+    }
+
+    var iB = Math.pow(10, iCount);
+
+    //有时乘100结果也不精确
+    var value1 = srcValue * iB;
+
+    var anumber = [];
+    var anumber1 = [];
+
+    var fvalue = value1;
+    var value2 = value1.toString();
+    var idot = value2.indexOf(".");
+
+    //如果是小数
+    if ( idot != -1 ) {
+        anumber = srcValue.toString().split(".");
+
+        //如果是科学计数法结果
+        if ( anumber[1].indexOf("e") != -1 ) {
+            return Math.round(value1) / iB;
+        }
+
+        anumber1=value2.split(".");
+
+        if ( anumber[1].length <= iCount ) {
+            return parseFloat(srcValuef,10);
+        }
+
+        var fvalue3 = parseInt(anumber[1].substring(iCount,iCount+1),10);
+
+        if ( fvalue3 >= 5 ) {
+            fvalue = parseInt(anumber1[0],10) + 1;
+        } else {
+            //对于传入的形如111.834999999998 的处理（传入的计算结果就是错误的，应为111.835）
+            if ( fvalue3 == 4 && anumber[1].length > 10 && parseInt(anumber[1].substring(iCount+1,iCount+2),10) == 9 ) {
+                fvalue = parseInt(anumber1[0],10) + 1;
+            } else {
+                fvalue = parseInt(anumber1[0],10);
+            }
+        }
+    }
+
+    //如果是负数就用0减四舍五入的绝对值
+    if ( zs ) {
+        return fvalue / iB;
+    } else {
+        return 0 - fvalue / iB;
+    }
+}
+
+//判断对象是否为空
+function isEmpty(src) {
+    return undefined == src || null == src || "" == src;
+}
+
+//date格式化
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S": this.getMilliseconds()
+    };
+
+    if ( /(y+)/.test(format) ) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for ( var k in o ) {
+        if ( new RegExp("(" + k + ")").test(format) ) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+
+    return format;
+};
+
+//判断是否为IE浏览器
+function isIE() {
+    if ( isEmpty(window) ) {
+        return false;
+    }
+
+    //判断是否为IE浏览器
+    if ( navigator.userAgent.indexOf("MSIE") > 0 ) {
+        return true;
+    } else {
+        return "ActiveXObject" in window;
+    }
+}
+
+//获取当前event对象
+function getEvent() {
+    var e;
+
+    if ( !isIE() ) {
+        if ( isEmpty(arguments.callee.caller) ) {
+            e = arguments.callee.arguments[0];
+        } else {
+            e = arguments.callee.caller.arguments[0];
+        }
+    } else {
+        e = window.event;
+    }
+
+    return e;
+}
+
+//阻止父层事件触发
+function preventTopEvent(e) {
+    e = getEvent();
+
+    if ( isIE() ) {
+        e.cancelBubble = true;
+    } else {
+        e.stopPropagation();
+    }
+
+    e = null;
+}
+
+//获取按键对应的值
+function getKeyCode(e) {
+    var key = "";
+
+    if ( isIE() ) {
+        key = e.keyCode;
+    } else {
+        key = e.which;
+    }
+
+    return key;
+}
